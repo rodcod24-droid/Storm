0e1188ceca77beb82a322cd55947697880450c67
rodcod24-droid <rodcod24@gmail.com>
2025-08-03 19:12:16 -0700

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,1 +1,8 @@
-no
+{
+    "name": "Rodo providers repository",
+    "description": "Cloudstream extension Repository",
+    "manifestVersion": 1,
+    "pluginLists": [
+      "https://raw.githubusercontent.com/rodcod24-droid/Storm/refs/heads/builds/plugins.json"
+    ]
+}
