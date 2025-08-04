c5cb4a1780d124665c776980db1dfd3da6645bfe
rodcod24-droid <rodcod24@gmail.com>
2025-08-03 19:32:32 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,11 @@ rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
 
-val disabled = listOf<String>()
+val disabled = listOf<String>(
+"CinecalidadProvider",
+"AnimeflvProvider",
+"AnimeflvIOProvider,
+)
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
